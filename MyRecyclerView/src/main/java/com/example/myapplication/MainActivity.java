c8b2a44912d355b276626e6fbb7ef20b40c643f5
Java-Coffee <1095908464@qq.com>
2016-09-06 22:36:43 +0800

    布局方式切换

--- MyRecyclerView/src/main/java/com/example/myapplication/MainActivity.java
+++ MyRecyclerView/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 import java.util.ArrayList;
@@ -27,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         //设置RecyclerView的布局管理
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         //设置RecyclerView的Item间分割线
-        mRecycleView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+        //mRecycleView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
         mRecycleView.setLayoutManager(linearLayoutManager);
     }
 
@@ -41,5 +45,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         mRecycleView = (RecyclerView) findViewById(R.id.recyclerView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_gridview:
+                mRecycleView.setLayoutManager(new GridLayoutManager(this,3));
+                break;
+            case R.id.action_listview:
+                mRecycleView.setLayoutManager(new LinearLayoutManager(this));
+                break;
+            case R.id.action_hor_gridview:
+                mRecycleView.setLayoutManager(new StaggeredGridLayoutManager(5,StaggeredGridLayoutManager.HORIZONTAL));
+                break;
+            case R.id.action_staggered:
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
